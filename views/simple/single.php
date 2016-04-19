37989c3d9d23fea94361922d2a65abfa02fc22ae
Dharmendra Prajapati <dharmendra.prajapati6565@gmail.com>
2016-04-19 19:59:34 +0530

    Applied quick fixes.

--- views/simple/single.php
+++ views/simple/single.php
@@ -1,4 +1,7 @@
 <?php
+// Yii Imports
+use \Yii;
+use yii\helpers\Html;
 
 // CMG Imports
 use cmsgears\core\common\utilities\DateUtil;
@@ -6,7 +9,7 @@ use cmsgears\core\common\utilities\DateUtil;
 $rating			= $model->rating;
 $commentDate	= DateUtil::getDateFromDateTime( $model->createdAt );
 ?>
-<div class="clearfix">
+<div <?= Html::renderTagAttributes( $widget->modelOptions ) ?>>
 	<div class="col12x2">
 		<img class="fluid circled1 " src="<?= Yii::getAlias( "@images" ) ?>/user-icon-2.png">
 		<p class="text bold italic font-size font-size-small align align-center"> <?= ucfirst( $model->name ) ?> </p>
--- views/submit/popup.php
+++ views/submit/popup.php
@@ -4,11 +4,11 @@
     <div class="popup-data popup-data-small">
         <span class="popup-close cmti cmti-close-c popup-close"></span>
         <div class="popup-wrap-title">
-            <span class="popup-title-text"><?= $title ?></span>
+            <span class="popup-title-text"><?= $widget->title ?></span>
         </div>
         <div class="popup-elements align align-center">
             <div class="content content-90">
-                <div class="form cmt-request" cmt-controller="<?= $controller ?>" cmt-action="<?= $action ?>" action="<?= $ajaxUrl ?>">
+                <div class="form cmt-request" cmt-controller="<?= $widget->controller ?>" cmt-action="<?= $widget->action ?>" action="<?= $widget->ajaxUrl ?>">
                     <textarea placeholder="Write here." name="ModelComment[content]"></textarea>
                     <div>
                         <span class='error' cmt-error='content'></span>
@@ -16,15 +16,15 @@
                     <?php if( isset( $user ) ) { ?>
                         <input type="hidden" name="ModelComment[name]" value="<?= $user->name ?>">
                         <input type="hidden" name="ModelComment[email]" value="<?= $user->email ?>">
-                        <input type="hidden" name="ModelComment[rating]" value="<?=$rating?>">
-                        <input type="hidden" name="ModelComment[parentId]" value="<?= $parentId ?>">
-                        <input type="hidden" name="ModelComment[parentType]" value="<?= $parentType ?>">
-                        <input type="hidden" name="ModelComment[type]" value="<?= $type ?>">
+                        <input type="hidden" name="ModelComment[rating]" value="<?=$widget->rating?>">
+                        <input type="hidden" name="ModelComment[parentId]" value="<?= $widget->parentId ?>">
+                        <input type="hidden" name="ModelComment[parentType]" value="<?= $widget->parentType ?>">
+                        <input type="hidden" name="ModelComment[type]" value="<?= $widget->type ?>">
                     <?php } ?>
                     <a class="btn btn-medium cmt-click">SUBMIT</a>
                  </div>
                  <div class="message hidden-primary">
-                     <h6><?= $successMessage ?></h6>
+                     <h6><?= $widget->successMessage ?></h6>
                  </div>
             </div>
         </div>
