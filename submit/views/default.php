95d8cdd3e2a744d349e3043b5b0d8f901f9e8ade
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-03-02 10:33:31 +0530

    Applied quick fixes.

--- submit/views/default.php
+++ submit/views/default.php
@@ -1,26 +1,26 @@
 <?php
-// Yii Imports
+// CMG Imports
 use cmsgears\core\common\base\Captcha;
 
-$model		= $widget->model;
-$captcha	= $widget->captcha;
-$title		= $widget->title;
-$success	= $widget->success;
-$rating		= $widget->rating;
+$model			= $widget->model;
+$captcha		= $widget->captcha;
+$title			= $widget->title;
+$success		= $widget->success;
+$rating			= $widget->rating;
+$ratingClass	= $widget->ratingClass;
 
-$user = Yii::$app->user->getIdentity();
+$user = Yii::$app->core->getUser();
 
 $ajaxUrl		= $widget->ajaxUrl;
 $cmtApp			= $widget->cmtApp;
 $cmtController	= $widget->cmtController;
 $cmtAction		= $widget->cmtAction;
 ?>
-<form id="frm-comment" class="form row max-cols-100" cmt-app="<?= $cmtApp ?>" cmt-controller="<?= $cmtController ?>" cmt-action="<?= $cmtAction ?>" action="<?= $ajaxUrl ?>">
+<form class="form row max-cols-100" cmt-app="<?= $cmtApp ?>" cmt-controller="<?= $cmtController ?>" cmt-action="<?= $cmtAction ?>" action="<?= $ajaxUrl ?>">
 	<div class="max-area-cover spinner">
 		<div class="valign-center cmti cmti-2x cmti-spinner-1 spin"></div>
 	</div>
-
-	<?php if( !isset( $user ) ) { ?>
+	<?php if( empty( $user ) ) { ?>
 	    <div class="col col12x6">
 	        <div class="frm-icon-element">
 	            <i class="icon fa fa-user"></i>
@@ -37,17 +37,16 @@ $cmtAction		= $widget->cmtAction;
 	    <div class="col col12x6">
 	        <div class="frm-icon-element">
 	            <i class="icon fa fa-user"></i>
-	            <input type="text" name="Comment[name]" placeholder="Name" value="<?= $user->username ?>" />
+				<input type="text" name="Comment[name]" placeholder="Name" value="<?= $user->getName() ?>" readonly="" />
 	        </div>
 	        <span class="error" cmt-error="name"></span>
 	        <div class="frm-icon-element">
 	            <i class="icon fa fa-at"></i>
-	            <input type="text" name="Comment[email]" placeholder="Email" value="<?= $user->email ?>" />
+	            <input type="text" name="Comment[email]" placeholder="Email" value="<?= $user->email ?>" readonly="" />
 	        </div>
 	        <span class="error" cmt-error="email"></span>
 	    </div>
     <?php } ?>
-
     <div class="col col12x6">
 		<div class="frm-icon-element">
 			<i class="icon fa fa-envelope-o"></i>
@@ -55,19 +54,15 @@ $cmtAction		= $widget->cmtAction;
 			<span class="error" cmt-error="content"></span>
 		</div>
     </div>
-
     <?php if( $widget->rating ) { ?>
 	    <div class="clear">
-	        <div class="box-rating rating-secondary clearfix">
-	            <?= Yii::$app->formDesigner->getRatingStars( null, 5, true ) ?>
-	        </div>
-			<input id="rating-count" type="hidden" name="Comment[rating]" />
+			<?= Yii::$app->formDesigner->getRatingField(
+				[ 'modelName' => 'Comment', 'fieldName' => 'rating', 'class' => $ratingClass ]
+			)?>
 			<span class="error" cmt-error="rating"></span>
 	    </div>
 	<?php } ?>
-
 	<div class="filler-height filler-height-medium"></div>
-
 	<?php if( !isset( $user ) ) { ?>
 		<div class="clear captcha-wrap">
 			<?= Captcha::widget( [ 'name' => 'Comment[captcha]', 'captchaAction' =>  '/core/site/captcha', 'options' => [ 'placeholder' => 'Captcha Key*' ] ] ) ?>
@@ -75,10 +70,10 @@ $cmtAction		= $widget->cmtAction;
 			<div class="error" cmt-error="captcha"></div>
 		</div>
 	<?php } ?>
-
     <input type="submit" class="frm-element-medium right" value="Submit" />
 
     <div class="filler-height"></div>
+
 	<div class="message success"></div>
 	<div class="message warning"></div>
 	<div class="message error"></div>
